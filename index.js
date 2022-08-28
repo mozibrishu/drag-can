26274b9f58962a771a60d405bf58e49682abdef1
Mozib <mozib.diu@gmail.com>
2022-08-28 15:45:25 +0600

    Animation Complete

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let textPng = document.querySelector(".textPng");
 let offerPng = document.querySelector(".offerPng");
 let bottom = document.querySelector(".bottom");
 dragElementLeft(canO);
+dragElementRight(canG);
 
 function dragElementLeft(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0, pos5 = 0, pos6 = 0;
@@ -66,6 +67,10 @@ function operationSecondSlide() {
     productPng.className = productPng.className.replace('hidden', '');
     textPng.classList ? textPng.classList.add('text-animation') : textPng.className += ' text-animation';
     offerPng.classList ? offerPng.classList.add('text-animation') : offerPng.className += ' text-animation';
+    productPng.classList ? productPng.classList.add('dbcan-animation') : productPng.className += ' dbcan-animation';
+    ctabtn.classList ? ctabtn.classList.add('pulse-animation') : ctabtn.className += ' pulse-animation';
+
+
 }
 
 function animationRotation(){
@@ -73,4 +78,50 @@ canO.className = canO.className.replace('animation-shake', '');
         canG.className = canG.className.replace('animation-shake', '');
         canO.classList ? canO.classList.add('rotateRight') : canO.className += ' rotateRight';
         canG.classList ? canG.classList.add('rotateLeft') : canG.className += ' rotateLeft';
+}
+
+function dragElementRight(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0, pos5 = 0, pos6 = 0;
+    var lElmnt = document.getElementById("canO");
+    if (document.getElementById(elmnt.id + "header")) {
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        animationRotation();
+
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        document.onmousemove = elementDrag;
+        document.onmouseup = closeDragElement;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+
+        pos3 = e.clientX;
+        leftSpace = elmnt.offsetLeft - pos1;
+        lLeftSpace = lElmnt.offsetLeft + pos1;
+
+        console.log(leftSpace);
+        if (leftSpace > 193) { leftSpace = 193 }
+        if (lLeftSpace < 43) { lLeftSpace = 43 }
+        if (leftSpace < 165) {
+            leftSpace = 165;
+            operationSecondSlide();
+        }
+        elmnt.style.left = leftSpace + "px";
+        lElmnt.style.left = lLeftSpace + "px";
+        console.log(lLeftSpace);
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
